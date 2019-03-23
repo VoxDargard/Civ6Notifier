2d981eedd7dfcd3c6794ceabbcfcbd97f5a8e152
Glitch (civhook) <none>
2019-03-23 19:28:23 +0000

    🍑🎅 Checkpoint ./server.js:3546442/1472 ./README.md:3546442/1281

--- README.md
+++ README.md
@@ -1,9 +1,12 @@
-# Multiple IFTTT Triggers
-IFTTT users have longed for the ability to trigger multiple actions from a single command, making it more like if-this-then-them. So you could for example, press a single button and trigger your lights to go off, your thermostat to turn off, and your security cameras to turn on.
+# Webhooks from Civ6 to discord with @mentions
 
-Well, with this app and IFTTT Webhooks you can make that happen!
+# How To:
+1. Remix this project
+2. Make it private so people can't see your usernames and such
+3. Go to server.js
+4. Add the players to the "playerMapping" array `(see below how to copy the discord id)`
+5. Add the game name and the corresponding Webhook url from discord to the "servermapping" array `(see below for more how to on that)`
+6. In the Civ settings (accessible thorugh the main menu) enter the webhook 
 
 ![](https://cdn.glitch.com/4761356a-9369-4e79-9d1e-a8306e8c00b5%2FifttMany.png)
 
-# Getting Started
-You need to create a Webhook that notifies your Glitch app, which in turn triggers multiple services in IFTTT. For step-by-step instructions, see `SETUP.md`.
--- server.js
+++ server.js
@@ -7,30 +7,19 @@ var upload = multer();
 app.use(express.static('views'));
 app.use(bodyParser.json());
 
-
+// Map the Steam name to the discord Id to @mention the players
 var playerMapping = {
-  somin: '287626849352286208',
-  Minski: '546389463052582933',
-  Forke: '274529125010767872',
-  Olovain: '284027402596646914',
-  TheLaggingMan: '362278244017504258',
-  DarkCiv: '327709450058334208',
-  'Lord Calderon': '366708961338195971',
-  'Dr.Friedlich_Loesung': '288766117827182604',
-  'Sol': '231450321925177354',
-  'Jesaya':'326480164827561984',
-  'funny_noodles':'362281149218947073'
-  
+  'steamName1': '12345DiscordId1234',
+  'steamName2': '12345DiscordId1234'
 };
 
+// Map game name to different bots so you can have channels per game
 var serverMapping = {
-  "Cloud-01": 'https://discordapp.com/api/webhooks/557468076115886110/xbdf2p2RFMJR4XFi-oCo1lDoaVX8lujLAQIZemtPT2BH9g9ly4m25t7FUSqORj3-vJK1',
-  'Cloud-02_CGG_KAKEBUKE': 'https://discordapp.com/api/webhooks/557822028015730703/kvzq_y2PCS0WKG5xJRB2Ay15vTOPn3pVXXUXsSap_IMlHC8jKT8Ls7TbbdAXkgNkcd-g',
-  "somin's Game": 'https://discordapp.com/api/webhooks/558026909577904139/_Fay9CuO_XDcspX_mmNXERgthwO1FRXx7XG7q8_XtE7JHvzlLvvZ0tuyYwrRnhVfPAM4',
-  "TestGame": 'https://discordapp.com/api/webhooks/558026909577904139/_Fay9CuO_XDcspX_mmNXERgthwO1FRXx7XG7q8_XtE7JHvzlLvvZ0tuyYwrRnhVfPAM4'
+  "CloudGameName": 'https://discordapp.com/...etc webhooklinkhere',
+  "DifferentCloudGame": 'https://discordapp.com/...etc webhooklinkhere'
 }; 
  
-var debugserver = 'https://discordapp.com/api/webhooks/558026909577904139/_Fay9CuO_XDcspX_mmNXERgthwO1FRXx7XG7q8_XtE7JHvzlLvvZ0tuyYwrRnhVfPAM4';
+var debugserver = 'https://...webhook link for when things go wrong';
 
 app.post("/", upload.array(),function (req, response) {
   
