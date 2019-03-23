b049fce68fcbbd3c4f399342c00da4cec988ef0a
Glitch (civhook) <none>
2019-03-23 10:56:45 +0000

    💉👕 Checkpoint ./server.js:3546442/981

--- server.js
+++ server.js
@@ -1,22 +1,9 @@
-// server.js
-// where your node app starts
-
-// init project
 var express = require('express');
 var bodyParser = require('body-parser');
 var request = require('request');
 var multer = require('multer'); // v1.0.5
-
 var app = express();
-
-var baseURL = "https://maker.ifttt.com/trigger/";
-var withKey = "/with/key/";
-var iftttId = "clazG-wwuSPmF8lae-fY3v";
-
 var upload = multer();
-
-// Show the homepage
-// http://expressjs.com/en/starter/static-files.html
 app.use(express.static('views'));
 app.use(bodyParser.json());
 
@@ -47,11 +34,6 @@ var serverMapping = {
 
 // Handle requests from IFTTT
 app.post("/", upload.array(),function (req, response) {
-  console.log("Request received from IFTTT");
-  console.log("Triggering multiple IFTTT services");
-//  for(var i=0; i<10; i++){
-    
-//  }
   
   console.log( req.body );
   console.log ( req.body.value2);
@@ -72,7 +54,6 @@ var listener = app.listen(process.env.PORT, function () {
 
 // Loops through each event and where it finds a value for it in .env it will make a request to IFTTT using it
 function checkForTrigger( server, playerId ){
-  var triggerEvent;
   
   request({ 
     uri: server,
@@ -88,18 +69,4 @@ function checkForTrigger( server, playerId ){
       console.log(body);
     }
   });
-  
-  /*
-  triggerEvent = "somin"
-  if(triggerEvent){
-    console.log("Event: " + triggerEvent + " Body: " + newBody);
-    // Make a request to baseURL + triggerEvent + withKey + iftttId, which is the complete IFTTT Maker Request URL
-    request(baseURL + triggerEvent + withKey + iftttId, function (error, response, body) {
-      if (!error && response.statusCode == 200) {
-        console.log(body); // Show the response from IFTTT
-      } else {
-        console.log(baseURL + triggerEvent + withKey + iftttId + ": "+error); // Show the error
-      }
-    });
-  }*/
 }
